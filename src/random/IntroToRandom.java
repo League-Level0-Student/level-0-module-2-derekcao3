8c611d46941e8eb6262b2b1c822d3e6f9d0946a4
Cao.Derek <Cao.Derek@SEMS-259-14.SEMS259.LAB>
2018-01-17 14:54:52 -0800

    IntroToRandom

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -1,4 +1,4 @@
-//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    The League of Amazing Programmers 2013-2017
 //    Level 0
 
 package random;
@@ -16,7 +16,9 @@ public class IntroToRandom {
 		//2. Now make r random. 
 		//   Create an object of the Random class
 		//   Hint: new Random().nextInt()
-
+		Random BlessRNG = new Random();
+		r =BlessRNG.nextInt((88+222)+1)-222;
+		System.out.println(r);
 		//3. Limit the random number between 0 and 100
 
 		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
